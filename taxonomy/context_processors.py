6e3b6285935cad86c4932cdb1bab24e7329df106
Mike Cornwell <mike@bonnieblueag.com>
2017-03-25 20:03:17 -0500

    Fixed bug.

--- taxonomy/context_processors.py
+++ taxonomy/context_processors.py
@@ -2,7 +2,7 @@ from taxonomy.models import UserTaxonomySettings
 
 def use_latin_names(request):
     useLatin = False
-    if request.user.is_authenticated:
+    if request.user.is_authenticated():
         settings = UserTaxonomySettings.objects.get_settings(request.user)
         useLatin = settings.use_latin_name
     return {'use_latin': useLatin}
